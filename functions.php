54342192075266a23cf6f0f31d984c4dc314286a
Bryan Stedman <bryanbstedman@gmail.com>
2016-04-21 16:44:42 -0700

    Add auto updating from github

--- functions.php
+++ functions.php
@@ -80,6 +80,37 @@ function garbanzo_setup() {
 endif;
 add_action( 'after_setup_theme', 'garbanzo_setup' );
 
+// Automatic theme updates from the GitHub repository
+add_filter('pre_set_site_transient_update_themes', 'automatic_GitHub_updates', 100, 1);
+function automatic_GitHub_updates($data) {
+  // Theme information
+  $theme   = get_stylesheet(); // Get the folder name of the current theme
+  $current = wp_get_theme()->get('Version'); // Get the version of the current theme
+  // GitHub information
+  $user = 'bryanstedman'; // The GitHub username hosting the repository
+  $repo = 'bug-free-octo-garbanzo'; // Repository name as it appears in the URL
+  // Get the latest release tag from the repository. The User-Agent header must be sent, as per
+  // GitHub's API documentation: https://developer.github.com/v3/#user-agent-required
+  $file = @json_decode(@file_get_contents('https://api.github.com/repos/'.$user.'/'.$repo.'/releases/latest', false,
+      stream_context_create(['http' => ['header' => "User-Agent: ".$user."\r\n"]])
+  ));
+  if($file) {
+    // Strip the version number of any non-alpha characters (excluding the period)
+    // This way you can still use tags like v1.1 or ver1.1 if desired
+    $update = filter_var($file->tag_name, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+    // Only return a response if the new version number is higher than the current version
+    if($update > $current) {
+      $data->response[$theme] = array(
+          'theme'       => $theme,
+          'new_version' => $update,
+          'url'         => 'https://github.com/'.$user.'/'.$repo,
+          'package'     => $file->assets[0]->browser_download_url,
+      );
+    }
+  }
+  return $data;
+}
+
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
